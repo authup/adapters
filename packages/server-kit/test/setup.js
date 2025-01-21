4fcfbe464ddb131ff93e56327ee7819374c8e473
tada5hi <peter.placzek1996@gmail.com>
2025-01-21 15:28:16 +0100

    chore: use node: prefix for crypto import

--- packages/server-kit/test/setup.js
+++ packages/server-kit/test/setup.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const crypto = require('crypto');
+const crypto = require('node:crypto');
 
 module.exports = () => {
     // eslint-disable-next-line no-undef
